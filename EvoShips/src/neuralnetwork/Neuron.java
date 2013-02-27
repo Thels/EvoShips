bd68f66ad3c7b45ae64ed060adb00fdc170d1f17
Ross Maden <ross.maden@strath.ac.uk>
2013-02-27 03:55:55 +0000

    Added in neuron code.
    Still to be refactored quite alot, not sure if a hashmap is the best way
    to do things, might try set max connections and deal with arrays
    instead.

--- EvoShips/src/neuralnetwork/Neuron.java
+++ EvoShips/src/neuralnetwork/Neuron.java
@@ -1,9 +1,93 @@
 package neuralnetwork;
 
-public class Neuron {
+import java.util.HashMap;
 
-	public Neuron() {
-		// TODO Auto-generated constructor stub
+public class Neuron 
+{
+	/*
+	 * For the current moment , neurons can have unlimited connections.
+	 */
+	private ENeuronTypes neuronType;
+	private double threshhold, weight, inputValue, neuronValue;
+	private HashMap<Neuron,Double> connectedWeightMap;
+	
+	public Neuron(ENeuronTypes neuronType, double threshhold) 
+	{
+		this.neuronType = neuronType;
+		this.threshhold = threshhold;
+		this.connectedWeightMap = new HashMap<Neuron,Double>();
 	}
+	
+	/**
+	 * Add a connection to this neuron. It places it in a hashmap, so that I can easily check what neurons it's connected to
+	 * and the respective weights of each of those connections.
+	 * @param otherNeuron Other neuron to connect to.
+	 * @param weight Weight of the connection.
+	 */
+	public void connectToNeuron(Neuron otherNeuron , double weight)
+	{
+		//Input neurons cannot be connected to anything. This shouldn't be called.
+		if(this.neuronType == ENeuronTypes.INPUT)
+			return;
+		connectedWeightMap.put(otherNeuron, weight);
+	}
+	
+	/**
+	 * Get the type of neuron this is as governed by ENeuronTypes.
+	 * @return Type of neuron ( INPUT / HIDDEN / OUTPUT ).
+	 */
+	public ENeuronTypes getNeuronType()
+	{
+		return this.neuronType;
+	}
+	
+	/**
+	 * Set the value of this neuron if it happens to be an input value.
+	 * 
+	 * The reason for this is because in order to start firing the neurons, the input values need to be set.
+	 * @param inputValue
+	 */
+	public void setInputValue(double inputValue)
+	{
+		if(this.neuronType == ENeuronTypes.INPUT)
+		{
+			this.inputValue = inputValue;
+		}
+	}
+	
+	/**
+	 * Get the value contained at this neuron.
+	 * 
+	 * Requires fire() to be called before value is set.
+	 * @return Value of the neuron.
+	 */
+	public double getNeuronValue()
+	{
+		return this.neuronValue;
+	}
+	
+	/**
+	 * Update this neuron by calculating it's value from the weight of it's given connections.
+	 */
+	public void updateNeuron()
+	{
+		if(getNeuronType() == ENeuronTypes.INPUT)
+		{
+			this.neuronValue = this.inputValue;
+			return;
+		}
+		
+		this.neuronValue = 0;
+		
+		for(Neuron n : this.connectedWeightMap.keySet())
+		{
+			neuronValue += connectedWeightMap.get(n)*n.getNeuronValue();
+		}
+		
+		neuronValue = Math.tanh(neuronValue);
+	}
+	
+	
+	
 
 }
