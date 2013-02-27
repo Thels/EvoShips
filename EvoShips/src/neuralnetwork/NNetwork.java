8b0eed03b6cb51e514d4c9ff6cd5f45d337f2016
Ross Laptop <ross.thels@gmail.com>
2013-02-27 04:04:46 +0000

    Started fleshing out the NN.

--- EvoShips/src/neuralnetwork/NNetwork.java
+++ EvoShips/src/neuralnetwork/NNetwork.java
@@ -8,16 +8,21 @@ package neuralnetwork;
 public class NNetwork 
 {
 
+	//Arrays the represent the neurons inside the neural network.
+	private Neuron inputNeurons[], outputNeurons[], hiddenNeurons[][];
+	
 	/**
 	 * Create a new neural network, with the given amount of input / hidden / output layers.
-	 * @param inputLayers Number of input layers.
-	 * @param hiddenLayers Number of hidden layers.
-	 * @param numberHiddenLayers Number of neurons in each hidden layer.
-	 * @param outputLayers Number of output layers.
+	 * @param inputCount Number of input layers.
+	 * @param hiddenCount Number of hidden neurons in each layer.
+	 * @param numberHiddenLayers Number of hidden layers that exist in the NN.
+	 * @param outputCount Number of output layers.
 	 */
-	public NNetwork(int inputLayers, int hiddenLayers, int numberHiddenLayers, int outputLayers) 
+	public NNetwork(int inputCount, int hiddenCount, int numberHiddenLayers, int outputCount) 
 	{
-		
+		inputNeurons = new Neuron[inputCount];
+		outputNeurons = new Neuron[outputCount];
+		hiddenNeurons = new Neuron[numberHiddenLayers][hiddenCount];
 	}
 
 }
