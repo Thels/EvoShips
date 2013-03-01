1c53af9fb9cca9ba7c7ccad9c3106d70365912ec
Ross Uni <ross.maden@strath.ac.uk>
2013-03-01 09:02:58 +0000

    Network Outputs changes.
    Added a constructor for the enum to use, and a helper method to check
    whether or not a given neuron will activate the given output.

--- EvoShips/src/neuralnetwork/NetworkOutputs.java
+++ EvoShips/src/neuralnetwork/NetworkOutputs.java
@@ -7,9 +7,30 @@ package neuralnetwork;
  */
 public enum NetworkOutputs 
 {
-	MOVE_FORWARD,
-	MOVE_BACKWARD,
-	TURN_LEFT,
-	TURN_RIGHT,
-	FIRE;
+	MOVE_FORWARD(0.05),
+	MOVE_BACKWARD(0.05),
+	TURN_LEFT(0.05),
+	TURN_RIGHT(0.05),
+	FIRE(0.01);
+	
+	private double activationThreshold;
+	
+	/**
+	 * Creates a new network output, with a given activation threshold.
+	 * @param activationThreshold Activation threshold for this output.
+	 */
+	NetworkOutputs(double activationThreshold)
+	{
+		this.activationThreshold = activationThreshold;
+	}
+	
+	/**
+	 * Check to see if a given value activates the given output.
+	 * @param value Value to check against the activation threshold.
+	 * @return Whether the value is greater than the activation threshold.
+	 */
+	public boolean isActivated(double value)
+	{
+		return value > this.activationThreshold;
+	}
 }
