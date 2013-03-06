0b1d3c64ff2649239ddf642ab41d8acbab574ae1
Ross Maden <ross.maden@strath.ac.uk>
2013-03-06 02:23:39 +0000

    UITest just creates a random NN ship.
    Nothing much more to add, it also only tests against one other ship.

--- EvoShips/src/ui/UITest.java
+++ EvoShips/src/ui/UITest.java
@@ -1,12 +1,19 @@
 package ui;
 
+import genetic.Chromosome;
+
 import java.awt.Dimension;
+
+import neuralnetwork.ENetworkInputs;
+import neuralnetwork.ENetworkOutputs;
 
 import arena.Arena;
 import arena.objects.AbstractShip;
 import arena.objects.ships.AsteroidHunter;
 import arena.objects.ships.AsteroidTurret;
+import arena.objects.ships.DerpShip;
 import arena.objects.ships.KamakazieShip;
+import arena.objects.ships.NNShip;
 import arena.objects.ships.Turret;
 
 public class UITest 
@@ -14,16 +21,20 @@ public class UITest
 
 	public UITest()
 	{
+		
+		int numberWeights = ( ENetworkInputs.values().length * 4 ) + 
+				(4 * 4 * (4-1)) + (4 * ENetworkOutputs.values().length);
 
-
+		Chromosome temp = new Chromosome(numberWeights);	
+		
+		NNShip test0 = new NNShip(temp);
 		AbstractShip test1 = new AsteroidHunter();
 		AbstractShip test2 = new AsteroidHunter();
 		AbstractShip test3 = new KamakazieShip();
 
 		Arena testGame = new Arena(20, 50, 5);
+		testGame.addShipToArena(test0);
 		testGame.addShipToArena(test1);
-		testGame.addShipToArena(test2);
-		testGame.addShipToArena(test3);
 		
 		ArenaFrame frame = new ArenaFrame(testGame, new Dimension(650,650));
 		
