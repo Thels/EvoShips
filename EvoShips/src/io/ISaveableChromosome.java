8d809c2d7e8b485a490c920a7adf5d5afad6925c
Ross Maden <ross.maden@strath.ac.uk>
2013-03-06 03:25:13 +0000

    ISaveableChromosome now takes a foldername.

--- EvoShips/src/io/ISaveableChromosome.java
+++ EvoShips/src/io/ISaveableChromosome.java
@@ -12,9 +12,10 @@ public interface ISaveableChromosome
 
 	/**
 	 * Save elements of a chromosome so that it can be loaded/edited later easily.
+	 * @param folderName Name of the folder the chromo has to be saved to.
 	 * @throws IOException Exception is thrown if there are any issues with file opening/creation/editing.
 	 */
-	public void saveToFile() throws IOException;
+	public void saveToFile(String folderName) throws IOException;
 	
 	
 }
