d5c7f4bcb35a823d62265d31c18d0a9df0053696
Ross Maden <ross.maden@strath.ac.uk>
2013-03-11 12:50:39 +0000

    NNShip completed.
    Added missing javadoc, removed missing import.

--- EvoShips/src/arena/objects/ships/NNShip.java
+++ EvoShips/src/arena/objects/ships/NNShip.java
@@ -4,7 +4,6 @@ import genetic.Chromosome;
 import neuralnetwork.ENetworkInputs;
 import neuralnetwork.ENetworkOutputs;
 import neuralnetwork.NNetwork;
-import arena.ArenaWatcher;
 import arena.objects.AbstractShip;
 
 public class NNShip extends AbstractShip 
@@ -17,6 +16,11 @@ public class NNShip extends AbstractShip
 	private final int HIDDEN_LAYER_COUNT = 4;
 	private final int NEURONS_PER_HIDDEN = 5;
 
+	/**
+	 * Create a new Neural Network ship. It creates the neural network based on the final fields above for the time being, if 
+	 * this project ended up having an extensive UI, then this ship would recieve the values for the NNetwork as additional parameters.
+	 * @param chromosome - The chromosome this NNShip is to base its actions upon.
+	 */
 	public NNShip(Chromosome chromosome) 
 	{
 		super();
@@ -38,10 +42,12 @@ public class NNShip extends AbstractShip
 		//Get the output from the updated neural network.
 		double[] networkOutputs = neuralNetwork.getNetworkOutputs();
 
+		//Moving forward is based on if the first output is greater than its bounds.
 		forward = outputs[0].isActivatedPositiveBound(networkOutputs[0]);
+		//Moving backward is based of if the first output is less than the negative lower bound.
 		backward = outputs[0].isActivatedNegativeBound(networkOutputs[0]);
 
-		
+		//Same for left and right.
 		left = outputs[1].isActivatedPositiveBound(networkOutputs[1]);
 		right = outputs[1].isActivatedNegativeBound(networkOutputs[1]);
 
