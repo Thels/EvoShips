75bb0bc29b99b779f17df2cb2a498898361b05ca
Ross Maden <ross.maden@strath.ac.uk>
2013-03-11 15:01:26 +0000

    Added GADriver for ease of running a GA.

---
+++ EvoShips/src/driver/GADriver.java
@@ -0,0 +1,55 @@
+package driver;
+
+import genetic.GA;
+
+import java.util.ArrayList;
+
+import arena.objects.AbstractShip;
+import arena.objects.ships.AsteroidHunter;
+
+/**
+ * Class used to easily run a genetic algorithm with a list of pre-set values.
+ * @author Ross
+ *
+ */
+public class GADriver 
+{
+	private int NUMBER_GENERATIONS = 200;
+	private int MAX_ASTEROIDS = 20;
+	private int ASTEROID_SPAWN_CHANCE = 1;
+	private int POPULATION_SIZE = 200;
+	private int GAMES_PER_GENERATION = 200;
+	
+	ArrayList<AbstractShip> otherShips;
+	
+	/**
+	 * Creates a new driver that will run with a set of ships and pre-defined values.
+	 * @param firstShip First ship to compete with the NNShip
+	 * @param secondShip Second ship to compete with the NNShip
+	 * @param thirdShip Third ship to compete with the NNShip
+	 */
+	public GADriver(AbstractShip firstShip, AbstractShip secondShip, AbstractShip thirdShip)
+	{
+		otherShips = new ArrayList<AbstractShip>();
+		
+		if(firstShip != null)
+			otherShips.add(firstShip);
+		if(secondShip != null)
+			otherShips.add(secondShip);
+		if(thirdShip != null)
+			otherShips.add(thirdShip);
+		
+		GA geneticAlgorithm = new GA(otherShips, POPULATION_SIZE, NUMBER_GENERATIONS, GAMES_PER_GENERATION, MAX_ASTEROIDS, ASTEROID_SPAWN_CHANCE);
+	}
+	
+	public static void main(String[] args)
+	{
+		AbstractShip other1 = new AsteroidHunter();
+		AbstractShip other2 = null;
+		AbstractShip other3 = null;
+		
+		new GADriver(other1, other2, other3);
+			
+	}
+
+}
