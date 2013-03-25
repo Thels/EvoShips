7adafbed392735c70329475803ac2bf8f71afe30
Ross Maden <ross.maden@strath.ac.uk>
2013-03-25 04:26:22 +0000

    GA renamed to GeneticAlgorithm

--- EvoShips/src/genetic/GeneticAlgorithm.java
+++ EvoShips/src/genetic/GeneticAlgorithm.java
@@ -1,5 +1,6 @@
 package genetic;
 
+import genetic.selectionAlgorithms.RouletteSection;
 import io.GeneticIO;
 
 import java.security.InvalidParameterException;
@@ -17,27 +18,23 @@ import arena.objects.ships.NNShip;
  * @author Ross
  *
  */
-public class GA 
+public class GeneticAlgorithm 
 {
 	private ArrayList<Chromosome> population;
 
+	//The chance when breeding a new chromosome that crossover will occur.
+	public static double CROSSOVER_CHANCE = 0.3;
+	//The chance when breeding a new chromosome that mutation will occur ( per allele ).
+	public static double MUTATION_CHANCE = 0.4;
 	//These are merely place holder values, if time permitted, a UI would be added that would allow the changing of these values.
 	private final int HIDDEN_LAYER_COUNT = 3;
 	private final int NEURONS_PER_HIDDEN = 9;
 
 	private int maxAsteroids, asteroidSpawnChance;
 
-	//Should elitism be used in this GA. Elitism is the copying of one or a few of some of the best chromosomes in each generation.
-	private final boolean ELITISM = true;
-	private final int ELITISM_COPY_COUNT = 1;
-
-	//The chance when breeding a new chromosome that crossover will occur.
-	private final double CROSSOVER_CHANCE = 0.3;
-	//The chance when breeding a new chromosome that mutation will occur ( per allele ).
-	private final double MUTATION_CHANCE = 0.4;
-
 	private GeneticIO geneticIOHandler;
 	private int generationCounter;
+	private ISelectionProcess selectionProcess;
 
 	/**
 	 * Creates a new genetic algorithm that will run with the given parameters.
@@ -54,7 +51,7 @@ public class GA
 	 * @param maxAsteroids Maximum number of asteroids in each of the arenas created by the GA.
 	 * @param asteroidSpawnChance % chance to spawn an asteroid in each arena per tick. ( 1-100 ).
 	 */
-	public GA(ArrayList<AbstractShip> otherShips, int populationSize, int generations, int gamesPerGeneration, int maxAsteroids, int asteroidSpawnChance) 
+	public GeneticAlgorithm(ArrayList<AbstractShip> otherShips, int populationSize, int generations, int gamesPerGeneration, int maxAsteroids, int asteroidSpawnChance) 
 	{
 		if(populationSize < 1 || generations < 1 || gamesPerGeneration < 1)
 			throw new InvalidParameterException("Invalid parameters for GA, must have >1 for values.");
@@ -63,7 +60,7 @@ public class GA
 		this.asteroidSpawnChance = asteroidSpawnChance;
 		
 		generationCounter = 0;
-
+		selectionProcess = new RouletteSection();
 		//Generate initial population.
 		population = new ArrayList<Chromosome>();
 		population = generateInitialPopulation(populationSize);
@@ -76,7 +73,7 @@ public class GA
 		for(int i = 0 ; i < generations; i++)
 		{
 			runGeneration(otherShips,population, gamesPerGeneration);
-			population = calculateNextPopulation(population);
+			population = selectionProcess.generateNewPopulation(population);
 		}
 
 	}
@@ -139,131 +136,13 @@ public class GA
 
 	}
 
-	/**
-	 * Calculate the next population given an input population.
-	 * @param inputPopulation Population to choose next populous from.
-	 * @return List of chromosomes that make up the next population.
-	 */
-	private ArrayList<Chromosome> calculateNextPopulation(ArrayList<Chromosome> inputPopulation) 
-	{
-		ArrayList<Chromosome> nextPopulation = new ArrayList<Chromosome>();
-
-		int populationCounter = 0;
-
-		/*
-		 * If elitish has been set to true, then the system will copy the top ELITISM_COPY_COUNT scoring chromosomes into
-		 * the new population, this is good to be able to retain the higher scoring ships in between each generation.
-		 * 
-		 * For elitism, I find the highest scoring chromosome, add it to the new population, and delete it from the input population.
-		 * This means that the next getElitist call wont return the same chromosome!
-		 */
-		if(ELITISM)
-		{
-			for(int i = 0 ; i < ELITISM_COPY_COUNT ; i++)
-			{
-				Chromosome eliteChromo = getElitist(inputPopulation);
-				nextPopulation.add(eliteChromo);
-				inputPopulation.remove(eliteChromo);
-				populationCounter++;
-			}
-		}
-
-		while(populationCounter < inputPopulation.size())
-		{
-			Chromosome dad, mum, child;
-			dad = rouletteChoice(inputPopulation);
-			mum = rouletteChoice(inputPopulation);
-
-			if(Math.random() < CROSSOVER_CHANCE)
-			{
-				child = GAHelper.breedChromosones(dad, mum);
-			}
-			else
-			{
-				//If crossover isn't taking place, then choose the child based on the mother or father.
-				if(Math.random() >= 0.5)
-					child = dad;
-				else
-					child = mum;
-			}
-
-			//Mutate the chromosome, small chance for each allele to be changed.
-			GAHelper.mutateChromosone(child, MUTATION_CHANCE);
-
-			nextPopulation.add(child);
-			populationCounter++;
-		}
-
-		return nextPopulation;
-	}
-
-	/**
-	 * Using roulette choice on a given population to select a given chromosome. Used to select a chromosome
-	 * that will represent the mother and father chromosomes.
-	 * 
-	 * The way roulette selection works, is using each chromosomes score as its probability to be chosen. To begin with I sum
-	 * the population, and choose a random number from 1-this sum. I then loop through the population, keeping track of a value which
-	 * each score gets added top, as soon as the total is above the choice, this means that this chromosome has been chosen. Through 
-	 * this method, chromosomes with a larger score have a larger chance to be selected.
-	 * 
-	 * @param inputPopulation Population of chromosomes to choose from.
-	 * @return A chromosome, selected through a process of probability.
-	 */
-	private Chromosome rouletteChoice(ArrayList<Chromosome> inputPopulation)
-	{
-		int populationSum = sumPopulation(population);
-		Random r = new Random();
-		int choice = r.nextInt(populationSum)+1;
-		int totalSoFar = 0;
-
-		for(Chromosome c : population)
-		{
-			totalSoFar += c.getChromosomeScore();
-			if(totalSoFar >= choice)
-				return c;
-		}
-
-		return null;
-	}
-
-	/**
-	 * Sum the scores of a given chromosome population.
-	 * @param population Population to sum.
-	 * @return Summation of all populations getScore().
-	 */
-	private int sumPopulation(ArrayList<Chromosome> population) 
-	{
-		int total = 0;
-		for(Chromosome c : population)
-			total += c.getChromosomeScore();
-		return total;
-	}
-
-	/**
-	 * Get the elitist chromosome of a given population. Which is the highest scoring of a given populace.
-	 * @param inputPopulation Population to check.
-	 * @return Highest scoring chromosome.
-	 */
-	private Chromosome getElitist(ArrayList<Chromosome> inputPopulation) 
-	{
-		Chromosome eliteChromosome = null;
-
-		int highestScore = 0;
-
-		for(Chromosome c : inputPopulation)
-		{
-			int cScore = c.getChromosomeScore();
-
-			if(cScore > highestScore)
-			{
-				eliteChromosome = c;
-				highestScore = cScore;
-			}
-		}
 
 
-		return new Chromosome(eliteChromosome.getWeights());
-	}
+	
+
+	
+
+	
 
 	/**
 	 * Reset all the scores of a given set of ships.
